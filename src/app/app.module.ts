18ea1366ac312360fba8483047e826d06a570c9c
shekar mudaliyar <sskshekar10@gmail.com>
2019-03-26 11:59:49 +0530

    firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AngularFireModule } from "angularfire2";
+import { config } from "./firebase.config";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  firebase: {
+    apiKey: "AIzaSyDir5gCTjlHspkw7DpA7jlLtrZ6u2kyV_A",
+    authDomain: "crested-idiom-216905.firebaseapp.com",
+    databaseURL: "https://crested-idiom-216905.firebaseio.com",
+    projectId: "crested-idiom-216905",
+    storageBucket: "crested-idiom-216905.appspot.com",
+    messagingSenderId: "306597045445"
+  }
+};
